01ef288e55dafc61eefc62ce87f242bda7069036
APIS <971888462@qq.com>
2021-01-28 18:12:59 +0800

    no message

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 
 ## 目录说明
 ├─ utils.js             常用工具函数
+
 ├─ regs.js              常用正则
+
 ├─ Nginx.conf           Nginx相关配置
+
+├─ Jenkins.conf         Jenkins相关配置
--- utils.js
+++ utils.js
@@ -62,30 +62,6 @@ export function isNumber(data) {
 }
 
 /**
- * 生成独立ID
- * @param {Number} n 生成的独立id的长度
- * @param {Array} arr 和将要生成的随机数作比较去重的id集合
- * @param {String} comb 可用于生成随机数id的字符集合
- */
-export const getUniqueId = (arr = [], n = 8, comb = '123456789') => {
-  const random = n => {
-    let str = comb;
-    let result = '';
-    for (let i = 0; i < n; i++) {
-      result += str[parseInt(Math.random() * str.length)];
-    }
-
-    if (arr.includes(result)) {
-      random(n);
-    } else {
-      return result;
-    }
-  };
-
-  return random(n);
-};
-
-/**
  * 函数去抖
  * @param {Function} fn 
  * @param {Number} delay
@@ -165,3 +141,27 @@ export const getRandom = (min, max, mode = 'rightOpen')=> {
  * @param {Nmuber} max 随机数右区间
  */
 export const getRandomInt = (min, max)=> Math.round(Math.random()*(max-min)) + min;
+
+/**
+ * 生成独立ID
+ * @param {Number} n 生成的独立id的长度
+ * @param {Array} arr 和将要生成的随机数作比较去重的id集合
+ * @param {String} comb 可用于生成随机数id的字符集合
+ */
+export const getUniqueId = (arr = [], n = 8, comb = '123456789') => {
+  const random = n => {
+    let str = comb;
+    let result = '';
+    for (let i = 0; i < n; i++) {
+      result += str[parseInt(Math.random() * str.length)];
+    }
+
+    if (arr.includes(result)) {
+      random(n);
+    } else {
+      return result;
+    }
+  };
+
+  return random(n);
+};
