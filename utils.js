d9149bc9259708d328a6e9a6074f067f0a0c2578
APIS <971888462@qq.com>
2021-02-04 18:05:41 +0800

    what： 新增Dom操作相关方法

--- utils.js
+++ utils.js
@@ -165,3 +165,68 @@ export const getUniqueId = (arr = [], n = 8, comb = '123456789') => {
 
   return random(n);
 };
+
+/**
+ * 参数字符串解析为对象
+ * @param {String} str 参数字符串 
+ */
+export const getSearchQuerys = (str)=>  {
+  if (!str) return;
+  let arrStrs = decodeURIComponent(str).split('&');
+  let objStrs = {};
+  for(let i = 0, len = arrStrs.length; i < len; i++) {
+    const [ key, value ] = arrStrs[i].split('=');
+    if (key !== undefined && key !== '') {
+      objStrs[key] = value;
+    }
+  }
+  return objStrs;
+};
+/**
+ * Dom操作相关
+ */
+export const EventUtils = { 
+	addEvent: function(element, type, handler) { 
+		if (element.addEventListener) { 
+			element.addEventListener(type, handler, false); 
+		} else if (element.attachEvent) { 
+			element.attachEvent("on" + type, handler); 
+		} else { 
+			element["on" + type] = handler; 
+		}
+	},
+	// 移除事件 
+	removeEvent: function(element, type, handler) { 
+		if (element.removeEventListener) { 
+			element.removeEventListener(type, handler, false); 
+		} else if (element.detachEvent) { 
+			element.detachEvent("on" + type, handler); 
+		} else { 
+			element["on" + type] = null; 
+		}
+	},
+	// 获取事件目标 
+	getTarget: function(event) { 
+		return event.target || event.srcElement; 
+	},
+	// 获取 event 对象的引用，取到事件的所有信息，确保随时能使用 event 
+	getEvent: function(event) { 
+		return event || window.event; 
+	},
+	// 阻止事件（主要是事件冒泡，因为 IE 不支持事件捕获） 
+	stopPropagation: function(event) { 
+		if (event.stopPropagation) { 
+			event.stopPropagation(); 
+		} else { 
+			event.cancelBubble = true;
+		}
+	},
+	// 取消事件的默认行为 
+	preventDefault: function(event) { 
+		if (event.preventDefault) { 
+			event.preventDefault(); 
+		} else { 
+			event.returnValue = false; 
+		} 
+	}
+};
